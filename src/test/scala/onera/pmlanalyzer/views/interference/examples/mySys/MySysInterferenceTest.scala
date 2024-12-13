602826d79377d331bb344ffd919046826953f2cd
kevin-delmas <kevin.delmas@onera.fr>
2024-12-13 16:35:29 +0100

    Add test of reduction factors

--- src/test/scala/onera/pmlanalyzer/views/interference/examples/mySys/MySysInterferenceTest.scala
+++ src/test/scala/onera/pmlanalyzer/views/interference/examples/mySys/MySysInterferenceTest.scala
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright (c)  2023. ONERA
+ * This file is part of PML Analyzer
+ *
+ * PML Analyzer is free software ;
+ * you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation ;
+ * either version 2 of  the License, or (at your option) any later version.
+ *
+ * PML Analyzer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program ;
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ ******************************************************************************/
+
 package onera.pmlanalyzer.views.interference.examples.mySys
 
 import onera.pmlanalyzer.pml.examples.mySys.MySysExport.MySys
@@ -21,5 +38,10 @@ class MySysInterferenceTest extends AnyFlatSpec with should.Matchers {
     if (diff.exists(_.nonEmpty)) {
       fail(diff.map(InterferenceTestExtension.failureMessage).mkString("\n"))
     }
+  }
+
+  "For MySys, the semantics reduction" should "consistent with paper" in {
+    MySys.computeSemanticReduction() shouldEqual (BigDecimal(37) / 17)
+    MySys.computeGraphReduction() shouldEqual (BigDecimal(71) / 28)
   }
 }
