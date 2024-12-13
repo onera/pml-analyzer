5935efc17f0ef020bcb8a32d094b35d1a922fbb2
kevin-delmas <kevin.delmas@onera.fr>
2024-12-13 17:30:12 +0100

    add first generated architectures

--- src/test/scala/onera/pmlanalyzer/pml/experiments/hbus/HbusCl2C4B8TransactionLibrary.scala
+++ src/test/scala/onera/pmlanalyzer/pml/experiments/hbus/HbusCl2C4B8TransactionLibrary.scala
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright (c)  2023. ONERA
+ * This file is part of PML Analyzer
+ *
+ * PML Analyzer is free software ;
+ * you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation ;
+ * either version 2 of  the License, or (at your option) any later version.
+ *
+ * PML Analyzer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program ;
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ ******************************************************************************/
+
 package onera.pmlanalyzer.pml.experiments.hbus
 
 import onera.pmlanalyzer.pml.model.configuration.TransactionLibrary
@@ -8,173 +25,181 @@ import scala.language.postfixOps
 trait HbusCl2C4B8TransactionLibrary extends TransactionLibrary {
   self: HbusCl2C4B8Platform with HbusCl2C4B8Software =>
 
+  val tCl0C0_L1_ld: Transaction = Transaction(app_Cl0C0 read Cl0.C0_L1)
+  tCl0C0_L1_ld.used
 
-  val tr_rosace_cg0_cl0_C0_l1_ld: Transaction = Transaction(app_rosace_cg0_cl0_C0 read rosace.cg0.cl0.C0_L1)
-  tr_rosace_cg0_cl0_C0_l1_ld.used
+  val tCl0C0_L1_st: Transaction = Transaction(app_Cl0C0 write Cl0.C0_L1)
+  tCl0C0_L1_st.used
 
-  val tr_rosace_cg0_cl0_C0_l1_st: Transaction = Transaction(app_rosace_cg0_cl0_C0 write rosace.cg0.cl0.C0_L1)
-  tr_rosace_cg0_cl0_C0_l1_st.used
+  val tCl0C0_L2_ld: Transaction = Transaction(app_Cl0C0 read Cl0.L2)
+  tCl0C0_L2_ld.used
 
-  val tr_rosace_cg0_cl0_C0_l2_ld: Transaction = Transaction(app_rosace_cg0_cl0_C0 read rosace.cg0.cl0.l2)
-  tr_rosace_cg0_cl0_C0_l2_ld.used
+  val tCl0C0_L2_st: Transaction = Transaction(app_Cl0C0 write Cl0.L2)
+  tCl0C0_L2_st.used
 
-  val tr_rosace_cg0_cl0_C0_l2_st: Transaction = Transaction(app_rosace_cg0_cl0_C0 write rosace.cg0.cl0.l2)
-  tr_rosace_cg0_cl0_C0_l2_st.used
+  val tCl0C0_BK0_ld: Transaction = Transaction(app_Cl0C0 read BK0)
+  tCl0C0_BK0_ld.used
 
-  val tr_rosace_cg0_cl0_C0_BK0_ld: Transaction = Transaction(app_rosace_cg0_cl0_C0 read rosace.ddr.BK0)
-  tr_rosace_cg0_cl0_C0_BK0_ld.used
+  val tCl0C0_BK0_st: Transaction = Transaction(app_Cl0C0 write BK0)
+  tCl0C0_BK0_st.used
 
-  val tr_rosace_cg0_cl0_C0_BK0_st: Transaction = Transaction(app_rosace_cg0_cl0_C0 write rosace.ddr.BK0)
-  tr_rosace_cg0_cl0_C0_BK0_st.used
+  val tCl0C1_L1_ld: Transaction = Transaction(app_Cl0C1 read Cl0.C1_L1)
+  tCl0C1_L1_ld.used
 
-  val tr_rosace_cg0_cl0_C1_l1_ld: Transaction = Transaction(app_rosace_cg0_cl0_C1 read rosace.cg0.cl0.C1_L1)
-  tr_rosace_cg0_cl0_C1_l1_ld.used
+  val tCl0C1_L1_st: Transaction = Transaction(app_Cl0C1 write Cl0.C1_L1)
+  tCl0C1_L1_st.used
 
-  val tr_rosace_cg0_cl0_C1_l1_st: Transaction = Transaction(app_rosace_cg0_cl0_C1 write rosace.cg0.cl0.C1_L1)
-  tr_rosace_cg0_cl0_C1_l1_st.used
+  val tCl0C1_L2_ld: Transaction = Transaction(app_Cl0C1 read Cl0.L2)
+  tCl0C1_L2_ld.used
 
-  val tr_rosace_cg0_cl0_C1_l2_ld: Transaction = Transaction(app_rosace_cg0_cl0_C1 read rosace.cg0.cl0.l2)
-  tr_rosace_cg0_cl0_C1_l2_ld.used
+  val tCl0C1_L2_st: Transaction = Transaction(app_Cl0C1 write Cl0.L2)
+  tCl0C1_L2_st.used
 
-  val tr_rosace_cg0_cl0_C1_l2_st: Transaction = Transaction(app_rosace_cg0_cl0_C1 write rosace.cg0.cl0.l2)
-  tr_rosace_cg0_cl0_C1_l2_st.used
+  val tCl0C1_BK1_ld: Transaction = Transaction(app_Cl0C1 read BK1)
+  tCl0C1_BK1_ld.used
 
-  val tr_rosace_cg0_cl0_C1_BK1_ld: Transaction = Transaction(app_rosace_cg0_cl0_C1 read rosace.ddr.BK1)
-  tr_rosace_cg0_cl0_C1_BK1_ld.used
+  val tCl0C1_BK1_st: Transaction = Transaction(app_Cl0C1 write BK1)
+  tCl0C1_BK1_st.used
 
-  val tr_rosace_cg0_cl0_C1_BK1_st: Transaction = Transaction(app_rosace_cg0_cl0_C1 write rosace.ddr.BK1)
-  tr_rosace_cg0_cl0_C1_BK1_st.used
+  val tCl0C2_L1_ld: Transaction = Transaction(app_Cl0C2 read Cl0.C2_L1)
+  tCl0C2_L1_ld.used
 
-  val tr_rosace_cg0_cl0_C2_l1_ld: Transaction = Transaction(app_rosace_cg0_cl0_C2 read rosace.cg0.cl0.C2_L1)
-  tr_rosace_cg0_cl0_C2_l1_ld.used
+  val tCl0C2_L1_st: Transaction = Transaction(app_Cl0C2 write Cl0.C2_L1)
+  tCl0C2_L1_st.used
 
-  val tr_rosace_cg0_cl0_C2_l1_st: Transaction = Transaction(app_rosace_cg0_cl0_C2 write rosace.cg0.cl0.C2_L1)
-  tr_rosace_cg0_cl0_C2_l1_st.used
+  val tCl0C2_L2_ld: Transaction = Transaction(app_Cl0C2 read Cl0.L2)
+  tCl0C2_L2_ld.used
 
-  val tr_rosace_cg0_cl0_C2_l2_ld: Transaction = Transaction(app_rosace_cg0_cl0_C2 read rosace.cg0.cl0.l2)
-  tr_rosace_cg0_cl0_C2_l2_ld.used
+  val tCl0C2_L2_st: Transaction = Transaction(app_Cl0C2 write Cl0.L2)
+  tCl0C2_L2_st.used
 
-  val tr_rosace_cg0_cl0_C2_l2_st: Transaction = Transaction(app_rosace_cg0_cl0_C2 write rosace.cg0.cl0.l2)
-  tr_rosace_cg0_cl0_C2_l2_st.used
+  val tCl0C2_BK2_ld: Transaction = Transaction(app_Cl0C2 read BK2)
+  tCl0C2_BK2_ld.used
 
-  val tr_rosace_cg0_cl0_C2_BK2_ld: Transaction = Transaction(app_rosace_cg0_cl0_C2 read rosace.ddr.BK2)
-  tr_rosace_cg0_cl0_C2_BK2_ld.used
+  val tCl0C2_BK2_st: Transaction = Transaction(app_Cl0C2 write BK2)
+  tCl0C2_BK2_st.used
 
-  val tr_rosace_cg0_cl0_C2_BK2_st: Transaction = Transaction(app_rosace_cg0_cl0_C2 write rosace.ddr.BK2)
-  tr_rosace_cg0_cl0_C2_BK2_st.used
+  val tCl0C3_L1_ld: Transaction = Transaction(app_Cl0C3 read Cl0.C3_L1)
+  tCl0C3_L1_ld.used
 
-  val tr_rosace_cg0_cl0_C3_l1_ld: Transaction = Transaction(app_rosace_cg0_cl0_C3 read rosace.cg0.cl0.C3_L1)
-  tr_rosace_cg0_cl0_C3_l1_ld.used
+  val tCl0C3_L1_st: Transaction = Transaction(app_Cl0C3 write Cl0.C3_L1)
+  tCl0C3_L1_st.used
 
-  val tr_rosace_cg0_cl0_C3_l1_st: Transaction = Transaction(app_rosace_cg0_cl0_C3 write rosace.cg0.cl0.C3_L1)
-  tr_rosace_cg0_cl0_C3_l1_st.used
+  val tCl0C3_L2_ld: Transaction = Transaction(app_Cl0C3 read Cl0.L2)
+  tCl0C3_L2_ld.used
 
-  val tr_rosace_cg0_cl0_C3_l2_ld: Transaction = Transaction(app_rosace_cg0_cl0_C3 read rosace.cg0.cl0.l2)
-  tr_rosace_cg0_cl0_C3_l2_ld.used
+  val tCl0C3_L2_st: Transaction = Transaction(app_Cl0C3 write Cl0.L2)
+  tCl0C3_L2_st.used
 
-  val tr_rosace_cg0_cl0_C3_l2_st: Transaction = Transaction(app_rosace_cg0_cl0_C3 write rosace.cg0.cl0.l2)
-  tr_rosace_cg0_cl0_C3_l2_st.used
+  val tCl0C3_BK3_ld: Transaction = Transaction(app_Cl0C3 read BK3)
+  tCl0C3_BK3_ld.used
 
-  val tr_rosace_cg0_cl0_C3_BK3_ld: Transaction = Transaction(app_rosace_cg0_cl0_C3 read rosace.ddr.BK3)
-  tr_rosace_cg0_cl0_C3_BK3_ld.used
+  val tCl0C3_BK3_st: Transaction = Transaction(app_Cl0C3 write BK3)
+  tCl0C3_BK3_st.used
 
-  val tr_rosace_cg0_cl0_C3_BK3_st: Transaction = Transaction(app_rosace_cg0_cl0_C3 write rosace.ddr.BK3)
-  tr_rosace_cg0_cl0_C3_BK3_st.used
+  val tCl1C0_L1_ld: Transaction = Transaction(app_Cl1C0 read Cl1.C0_L1)
+  tCl1C0_L1_ld.used
 
-  val tr_rosace_cg0_cl1_C0_l1_ld: Transaction = Transaction(app_rosace_cg0_cl1_C0 read rosace.cg0.cl1.C0_L1)
-  tr_rosace_cg0_cl1_C0_l1_ld.used
+  val tCl1C0_L1_st: Transaction = Transaction(app_Cl1C0 write Cl1.C0_L1)
+  tCl1C0_L1_st.used
 
-  val tr_rosace_cg0_cl1_C0_l1_st: Transaction = Transaction(app_rosace_cg0_cl1_C0 write rosace.cg0.cl1.C0_L1)
-  tr_rosace_cg0_cl1_C0_l1_st.used
+  val tCl1C0_L2_ld: Transaction = Transaction(app_Cl1C0 read Cl1.L2)
+  tCl1C0_L2_ld.used
 
-  val tr_rosace_cg0_cl1_C0_l2_ld: Transaction = Transaction(app_rosace_cg0_cl1_C0 read rosace.cg0.cl1.l2)
-  tr_rosace_cg0_cl1_C0_l2_ld.used
+  val tCl1C0_L2_st: Transaction = Transaction(app_Cl1C0 write Cl1.L2)
+  tCl1C0_L2_st.used
 
-  val tr_rosace_cg0_cl1_C0_l2_st: Transaction = Transaction(app_rosace_cg0_cl1_C0 write rosace.cg0.cl1.l2)
-  tr_rosace_cg0_cl1_C0_l2_st.used
+  val tCl1C0_BK4_ld: Transaction = Transaction(app_Cl1C0 read BK4)
+  tCl1C0_BK4_ld.used
 
-  val tr_rosace_cg0_cl1_C0_BK4_ld: Transaction = Transaction(app_rosace_cg0_cl1_C0 read rosace.ddr.BK4)
-  tr_rosace_cg0_cl1_C0_BK4_ld.used
+  val tCl1C0_BK4_st: Transaction = Transaction(app_Cl1C0 write BK4)
+  tCl1C0_BK4_st.used
 
-  val tr_rosace_cg0_cl1_C0_BK4_st: Transaction = Transaction(app_rosace_cg0_cl1_C0 write rosace.ddr.BK4)
-  tr_rosace_cg0_cl1_C0_BK4_st.used
+  val tCl1C1_L1_ld: Transaction = Transaction(app_Cl1C1 read Cl1.C1_L1)
+  tCl1C1_L1_ld.used
 
-  val tr_rosace_cg0_cl1_C1_l1_ld: Transaction = Transaction(app_rosace_cg0_cl1_C1 read rosace.cg0.cl1.C1_L1)
-  tr_rosace_cg0_cl1_C1_l1_ld.used
+  val tCl1C1_L1_st: Transaction = Transaction(app_Cl1C1 write Cl1.C1_L1)
+  tCl1C1_L1_st.used
 
-  val tr_rosace_cg0_cl1_C1_l1_st: Transaction = Transaction(app_rosace_cg0_cl1_C1 write rosace.cg0.cl1.C1_L1)
-  tr_rosace_cg0_cl1_C1_l1_st.used
+  val tCl1C1_L2_ld: Transaction = Transaction(app_Cl1C1 read Cl1.L2)
+  tCl1C1_L2_ld.used
 
-  val tr_rosace_cg0_cl1_C1_l2_ld: Transaction = Transaction(app_rosace_cg0_cl1_C1 read rosace.cg0.cl1.l2)
-  tr_rosace_cg0_cl1_C1_l2_ld.used
+  val tCl1C1_L2_st: Transaction = Transaction(app_Cl1C1 write Cl1.L2)
+  tCl1C1_L2_st.used
 
-  val tr_rosace_cg0_cl1_C1_l2_st: Transaction = Transaction(app_rosace_cg0_cl1_C1 write rosace.cg0.cl1.l2)
-  tr_rosace_cg0_cl1_C1_l2_st.used
+  val tCl1C1_BK5_ld: Transaction = Transaction(app_Cl1C1 read BK5)
+  tCl1C1_BK5_ld.used
 
-  val tr_rosace_cg0_cl1_C1_BK5_ld: Transaction = Transaction(app_rosace_cg0_cl1_C1 read rosace.ddr.BK5)
-  tr_rosace_cg0_cl1_C1_BK5_ld.used
+  val tCl1C1_BK5_st: Transaction = Transaction(app_Cl1C1 write BK5)
+  tCl1C1_BK5_st.used
 
-  val tr_rosace_cg0_cl1_C1_BK5_st: Transaction = Transaction(app_rosace_cg0_cl1_C1 write rosace.ddr.BK5)
-  tr_rosace_cg0_cl1_C1_BK5_st.used
+  val tCl1C2_L1_ld: Transaction = Transaction(app_Cl1C2 read Cl1.C2_L1)
+  tCl1C2_L1_ld.used
 
-  val tr_rosace_cg0_cl1_C2_l1_ld: Transaction = Transaction(app_rosace_cg0_cl1_C2 read rosace.cg0.cl1.C2_L1)
-  tr_rosace_cg0_cl1_C2_l1_ld.used
+  val tCl1C2_L1_st: Transaction = Transaction(app_Cl1C2 write Cl1.C2_L1)
+  tCl1C2_L1_st.used
 
-  val tr_rosace_cg0_cl1_C2_l1_st: Transaction = Transaction(app_rosace_cg0_cl1_C2 write rosace.cg0.cl1.C2_L1)
-  tr_rosace_cg0_cl1_C2_l1_st.used
+  val tCl1C2_L2_ld: Transaction = Transaction(app_Cl1C2 read Cl1.L2)
+  tCl1C2_L2_ld.used
 
-  val tr_rosace_cg0_cl1_C2_l2_ld: Transaction = Transaction(app_rosace_cg0_cl1_C2 read rosace.cg0.cl1.l2)
-  tr_rosace_cg0_cl1_C2_l2_ld.used
+  val tCl1C2_L2_st: Transaction = Transaction(app_Cl1C2 write Cl1.L2)
+  tCl1C2_L2_st.used
 
-  val tr_rosace_cg0_cl1_C2_l2_st: Transaction = Transaction(app_rosace_cg0_cl1_C2 write rosace.cg0.cl1.l2)
-  tr_rosace_cg0_cl1_C2_l2_st.used
+  val tCl1C2_BK6_ld: Transaction = Transaction(app_Cl1C2 read BK6)
+  tCl1C2_BK6_ld.used
 
-  val tr_rosace_cg0_cl1_C2_BK6_ld: Transaction = Transaction(app_rosace_cg0_cl1_C2 read rosace.ddr.BK6)
-  tr_rosace_cg0_cl1_C2_BK6_ld.used
+  val tCl1C2_BK6_st: Transaction = Transaction(app_Cl1C2 write BK6)
+  tCl1C2_BK6_st.used
 
-  val tr_rosace_cg0_cl1_C2_BK6_st: Transaction = Transaction(app_rosace_cg0_cl1_C2 write rosace.ddr.BK6)
-  tr_rosace_cg0_cl1_C2_BK6_st.used
+  val tCl1C3_L1_ld: Transaction = Transaction(app_Cl1C3 read Cl1.C3_L1)
+  tCl1C3_L1_ld.used
 
-  val tr_rosace_cg0_cl1_C3_l1_ld: Transaction = Transaction(app_rosace_cg0_cl1_C3 read rosace.cg0.cl1.C3_L1)
-  tr_rosace_cg0_cl1_C3_l1_ld.used
+  val tCl1C3_L1_st: Transaction = Transaction(app_Cl1C3 write Cl1.C3_L1)
+  tCl1C3_L1_st.used
 
-  val tr_rosace_cg0_cl1_C3_l1_st: Transaction = Transaction(app_rosace_cg0_cl1_C3 write rosace.cg0.cl1.C3_L1)
-  tr_rosace_cg0_cl1_C3_l1_st.used
+  val tCl1C3_L2_ld: Transaction = Transaction(app_Cl1C3 read Cl1.L2)
+  tCl1C3_L2_ld.used
 
-  val tr_rosace_cg0_cl1_C3_l2_ld: Transaction = Transaction(app_rosace_cg0_cl1_C3 read rosace.cg0.cl1.l2)
-  tr_rosace_cg0_cl1_C3_l2_ld.used
+  val tCl1C3_L2_st: Transaction = Transaction(app_Cl1C3 write Cl1.L2)
+  tCl1C3_L2_st.used
 
-  val tr_rosace_cg0_cl1_C3_l2_st: Transaction = Transaction(app_rosace_cg0_cl1_C3 write rosace.cg0.cl1.l2)
-  tr_rosace_cg0_cl1_C3_l2_st.used
+  val tCl1C3_BK7_ld: Transaction = Transaction(app_Cl1C3 read BK7)
+  tCl1C3_BK7_ld.used
 
-  val tr_rosace_cg0_cl1_C3_BK7_ld: Transaction = Transaction(app_rosace_cg0_cl1_C3 read rosace.ddr.BK7)
-  tr_rosace_cg0_cl1_C3_BK7_ld.used
+  val tCl1C3_BK7_st: Transaction = Transaction(app_Cl1C3 write BK7)
+  tCl1C3_BK7_st.used
 
-  val tr_rosace_cg0_cl1_C3_BK7_st: Transaction = Transaction(app_rosace_cg0_cl1_C3 write rosace.ddr.BK7)
-  tr_rosace_cg0_cl1_C3_BK7_st.used
+  val tC0_DMAReg_ld: Transaction = Transaction(app_Cl0C0 read DMAReg)
+  tC0_DMAReg_ld.used
 
-  val tr_CIO_dma_reg_ld: Transaction = Transaction(app_rosace_cg0_cl0_C0 read rosace.cfg_bus.dma_reg)
-  tr_CIO_dma_reg_ld.used
+  val tC0_DMAReg_st: Transaction = Transaction(app_Cl0C0 write DMAReg)
+  tC0_DMAReg_st.used
 
-  val tr_CIO_dma_reg_st: Transaction = Transaction(app_rosace_cg0_cl0_C0 write rosace.cfg_bus.dma_reg)
-  tr_CIO_dma_reg_st.used
+  val tC0_BK2_ld: Transaction = Transaction(app_Cl0C0 read BK2)
+  tC0_BK2_ld.used
 
-  val tr_CIO_BK0_ld: Transaction = Transaction(app_rosace_cg0_cl0_C0 read rosace.ddr.BK0)
-  tr_CIO_BK0_ld.used
+  val tC0_BK2_st: Transaction = Transaction(app_Cl0C0 write BK2)
+  tC0_BK2_st.used
 
-  val tr_CIO_BK0_st: Transaction = Transaction(app_rosace_cg0_cl0_C0 write rosace.ddr.BK0)
-  tr_CIO_BK0_st.used
+  val tC0_BK4_ld: Transaction = Transaction(app_Cl0C0 read BK4)
+  tC0_BK4_ld.used
 
-  val tr_dma_BK0_eth: Scenario = Scenario(app_dma read rosace.ddr.BK0, app_dma write rosace.eth)
-  tr_dma_BK0_eth.used
+  val tC0_BK4_st: Transaction = Transaction(app_Cl0C0 write BK4)
+  tC0_BK4_st.used
 
-  val tr_dma_eth_BK2: Scenario = Scenario(app_dma write rosace.ddr.BK2, app_dma read rosace.eth)
-  tr_dma_eth_BK2.used
+  val tC0_BK6_ld: Transaction = Transaction(app_Cl0C0 read BK6)
+  tC0_BK6_ld.used
 
-  val tr_dma_eth_BK4: Scenario = Scenario(app_dma write rosace.ddr.BK4, app_dma read rosace.eth)
-  tr_dma_eth_BK4.used
+  val tC0_BK6_st: Transaction = Transaction(app_Cl0C0 write BK6)
+  tC0_BK6_st.used
 
-  val tr_dma_eth_BK6: Scenario = Scenario(app_dma write rosace.ddr.BK6, app_dma read rosace.eth)
-  tr_dma_eth_BK6.used
+  val tDMA_BK2_ETH: Scenario = Scenario(app_dma read BK2, app_dma write ETH)
+  tDMA_BK2_ETH.used
+
+  val tDMA_BK4_ETH: Scenario = Scenario(app_dma write BK4, app_dma read ETH)
+  tDMA_BK4_ETH.used
+
+  val tDMA_BK6_ETH: Scenario = Scenario(app_dma write BK6, app_dma read ETH)
+  tDMA_BK6_ETH.used
 
 }
