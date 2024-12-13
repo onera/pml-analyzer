3dbb68288eca084ec1346229a85ef962de0183fe
kevin-delmas <kevin.delmas@onera.fr>
2024-12-13 16:54:35 +0100

    solver linter issue

--- src/main/scala/onera/pmlanalyzer/views/dependability/exporters/TargetCeciliaExporter.scala
+++ src/main/scala/onera/pmlanalyzer/views/dependability/exporters/TargetCeciliaExporter.scala
@@ -1,20 +1,19 @@
-/** *****************************************************************************
-  * Copyright (c) 2023. ONERA This file is part of PML Analyzer
-  *
-  * PML Analyzer is free software ; you can redistribute it and/or modify it
-  * under the terms of the GNU Lesser General Public License as published by the
-  * Free Software Foundation ; either version 2 of the License, or (at your
-  * option) any later version.
-  *
-  * PML Analyzer is distributed in the hope that it will be useful, but WITHOUT
-  * ANY WARRANTY ; without even the implied warranty of MERCHANTABILITY or
-  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
-  * for more details.
-  *
-  * You should have received a copy of the GNU Lesser General Public License
-  * along with this program ; if not, write to the Free Software Foundation,
-  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-  */
+/*******************************************************************************
+ * Copyright (c)  2023. ONERA
+ * This file is part of PML Analyzer
+ *
+ * PML Analyzer is free software ;
+ * you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation ;
+ * either version 2 of  the License, or (at your option) any later version.
+ *
+ * PML Analyzer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program ;
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ ******************************************************************************/
 
 package onera.pmlanalyzer.views.dependability.exporters
 
@@ -107,7 +106,14 @@ trait TargetCeciliaExporter {
               .distinct
               .sorted
               .mkString("\n")}
-           |${obsSub.values.flatMap(_._2).toList.distinct.sorted.mkString("\n")}
+           |${
+            obsSub.values
+              .flatMap(_._2)
+              .toList
+              .distinct
+              .sorted
+              .mkString("\n")
+          }
            |
            |// observator of store states according to component state
            |${obsSub
@@ -171,7 +177,14 @@ trait TargetCeciliaExporter {
           Nil,
           s"""assert
            |// subcomponent assertions
-           |${obsSub.values.flatMap(_._2).toList.distinct.sorted.mkString("\n")}
+           |${
+            obsSub.values
+              .flatMap(_._2)
+              .toList
+              .distinct
+              .sorted
+              .mkString("\n")
+          }
            |
            |// resulting store assertions
            |${obsSub
