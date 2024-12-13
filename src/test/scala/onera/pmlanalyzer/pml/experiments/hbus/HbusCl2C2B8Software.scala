03f5ec6af4815001a1d85cfb4a39fb2bdf59eb12
kevin-delmas <kevin.delmas@onera.fr>
2024-12-13 17:30:12 +0100

    add first generated architectures

--- src/test/scala/onera/pmlanalyzer/pml/experiments/hbus/HbusCl2C2B8Software.scala
+++ src/test/scala/onera/pmlanalyzer/pml/experiments/hbus/HbusCl2C2B8Software.scala
@@ -1,24 +1,39 @@
+/*******************************************************************************
+ * Copyright (c)  2023. ONERA
+ * This file is part of PML Analyzer
+ *
+ * PML Analyzer is free software ;
+ * you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation ;
+ * either version 2 of  the License, or (at your option) any later version.
+ *
+ * PML Analyzer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program ;
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ ******************************************************************************/
+
 package onera.pmlanalyzer.pml.experiments.hbus
 
 import onera.pmlanalyzer.pml.model.software.{Application, Data}
 import onera.pmlanalyzer.pml.operators.*
 
+import scala.language.postfixOps
+
 trait HbusCl2C2B8Software {
   self: HbusCl2C2B8Platform =>
 
-  val app_rosace_cg0_cl0_C0: Application = Application()
-  app_rosace_cg0_cl0_C0 hostedBy rosace.cg0.cl0.C0
-
-  val app_rosace_cg0_cl0_C1: Application = Application()
-  app_rosace_cg0_cl0_C1 hostedBy rosace.cg0.cl0.C1
-
-  val app_rosace_cg0_cl1_C0: Application = Application()
-  app_rosace_cg0_cl1_C0 hostedBy rosace.cg0.cl1.C0
-
-  val app_rosace_cg0_cl1_C1: Application = Application()
-  app_rosace_cg0_cl1_C1 hostedBy rosace.cg0.cl1.C1
-
+  val app_Cl0C0: Application = Application()
+  app_Cl0C0 hostedBy Cl0.C0
+  val app_Cl0C1: Application = Application()
+  app_Cl0C1 hostedBy Cl0.C1
+  val app_Cl1C0: Application = Application()
+  app_Cl1C0 hostedBy Cl1.C0
+  val app_Cl1C1: Application = Application()
+  app_Cl1C1 hostedBy Cl1.C1
   val app_dma: Application = Application()
-  app_dma hostedBy rosace.dma
+  app_dma hostedBy DMA
 
 }
