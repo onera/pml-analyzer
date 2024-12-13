39d6d2f096bb3f2f76dc314fe8e18145abbe6e56
kevin-delmas <kevin.delmas@onera.fr>
2024-12-13 16:27:19 +0100

    Add reduction factor computation

--- src/test/scala/onera/pmlanalyzer/views/interference/InterferenceTestExtension.scala
+++ src/test/scala/onera/pmlanalyzer/views/interference/InterferenceTestExtension.scala
@@ -1,21 +1,19 @@
-/** *****************************************************************************
-  * Copyright (c) 2021. ONERA This file is part of PML Analyzer
-  *
-  * PML Analyzer is free software ; you can redistribute it and/or modify it
-  * under the terms of the GNU Lesser General Public License as published by the
-  * Free Software Foundation ; either version 2 of the License, or (at your
-  * option) any later version.
-  *
-  * PML Analyzer is distributed in the hope that it will be useful, but WITHOUT
-  * ANY WARRANTY ; without even the implied warranty of MERCHANTABILITY or
-  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
-  * for more details.
-  *
-  * You should have received a copy of the GNU Lesser General Public License
-  * along with this program ; if not, write to the Free Software Foundation,
-  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-  * ****************************************************************************
-  */
+/*******************************************************************************
+ * Copyright (c)  2023. ONERA
+ * This file is part of PML Analyzer
+ *
+ * PML Analyzer is free software ;
+ * you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation ;
+ * either version 2 of  the License, or (at your option) any later version.
+ *
+ * PML Analyzer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program ;
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ ******************************************************************************/
 
 package onera.pmlanalyzer.views.interference
 
@@ -36,35 +34,26 @@ object InterferenceTestExtension {
 
     def computeGraphReduction(): BigDecimal = {
       val graph = x.fullServiceGraphWithInterfere()
-      val systemGraphSize = (graph.keySet ++ graph.values.flatten).size + graph
-        .flatMap(p => p._2 map { x => Set(p._1, x) })
-        .toSet
-        .size
+      val systemGraphSize = (graph.keySet ++ graph.values.flatten).size + graph.flatMap(p => p._2 map { x => Set(p._1, x) }).toSet.size
       println(s"System graph size is: $systemGraphSize")
       val (nodeSize, edgeSize) = x.getAnanlysisGraphSize()
       println(s"Interference channel graph size is: ${nodeSize + edgeSize}")
       BigDecimal(systemGraphSize) / BigDecimal(nodeSize + edgeSize)
     }
 
-    //    @deprecated("Inefficient implementation, should only count the model without storing them in files")
+    @deprecated("Inefficient implementation, should only count the model without storing them in files")
     def computeSemanticReduction(): BigDecimal = Await.result(
-      x.computeKInterference(
-        x.initiators.size,
-        ignoreExistingAnalysisFiles = true,
-        verboseResultFile = false
-      ).map(resultFiles =>
-        BigDecimal(x.getSemanticsSize.filter(_._1 >= 3).values.sum) /
-          (for {
-            i <- 3 to x.initiators.size
-            resultFile <- resultFiles
-              .find(_.getName == s"${x.fullName}_itf_$i.txt") ++
-              resultFiles.find(_.getName == s"${x.fullName}_free_$i.txt")
-          } yield {
-            PostProcess.parseScenarioFile(Source.fromFile(resultFile)).length
-          }).sum
-      ),
-      1 minute
-    )
+      x.computeKInterference(x.initiators.size, ignoreExistingAnalysisFiles = true, verboseResultFile = false)
+        .map(resultFiles =>
+          BigDecimal(x.getSemanticsSize.filter(_._1 >= 3).values.sum) /
+            (for {
+              i <- 3 to x.initiators.size
+              resultFile <- resultFiles.find(_.getName == s"${x.fullName}_itf_$i.txt") ++
+                resultFiles.find(_.getName == s"${x.fullName}_free_$i.txt")
+            } yield {
+              PostProcess.parseScenarioFile(Source.fromFile(resultFile)).length
+            }).sum),
+      1 minute)
 
     def test(
         max: Int,
@@ -73,24 +62,22 @@ object InterferenceTestExtension {
       x.computeKInterference(
         List(max, x.initiators.size).min,
         ignoreExistingAnalysisFiles = true,
-        computeSemantics = false,
-        verboseResultFile = false,
-        onlySummary = false
+        verboseResultFile = false
       ) map { resultFiles =>
         {
           for {
             i <- 2 to List(max, x.initiators.size).min
             fileITF <- FileManager.extractResource(
-              s"$expectedResultsDirectoryPath/${FileManager.getInterferenceAnalysisITFFileName(x, i)}"
+              s"$expectedResultsDirectoryPath/${x.fullName}_itf_$i.txt"
             )
             fileFree <- FileManager.extractResource(
-              s"$expectedResultsDirectoryPath/${FileManager.getInterferenceAnalysisFreeFileName(x, i)}"
+              s"$expectedResultsDirectoryPath/${x.fullName}_free_$i.txt"
             )
             rITFFile <- resultFiles.find(
-              _.getName == FileManager.getInterferenceAnalysisITFFileName(x, i)
+              _.getName == s"${x.fullName}_itf_$i.txt"
             )
             rFreeFile <- resultFiles.find(
-              _.getName == FileManager.getInterferenceAnalysisFreeFileName(x, i)
+              _.getName == s"${x.fullName}_free_$i.txt"
             )
           } yield {
             List(fileITF, fileFree)
