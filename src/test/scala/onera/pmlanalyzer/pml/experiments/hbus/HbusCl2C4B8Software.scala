7acfb002ac8a4205d71563f81c6629ff58fa8dfb
benjamin.lesage <benjamin.lesage@onera.fr>
2024-12-18 19:00:07 +0100

    Draft additional input ports to support writes to clusters

--- src/test/scala/onera/pmlanalyzer/pml/experiments/hbus/HbusCl2C4B8Software.scala
+++ src/test/scala/onera/pmlanalyzer/pml/experiments/hbus/HbusCl2C4B8Software.scala
@@ -3,10 +3,10 @@ package onera.pmlanalyzer.pml.experiments.hbus
 import onera.pmlanalyzer.pml.model.software.{Application, Data}
 import onera.pmlanalyzer.pml.operators.*
 
-import scala.language.postfixOps
 
 trait HbusCl2C4B8Software {
   self: HbusCl2C4B8Platform =>
+
 
   val app_rosace_cg0_cl0_C0: Application = Application()
   app_rosace_cg0_cl0_C0 hostedBy rosace.cg0.cl0.C0
@@ -34,5 +34,6 @@ trait HbusCl2C4B8Software {
 
   val app_dma: Application = Application()
   app_dma hostedBy rosace.dma
+
 
 }
