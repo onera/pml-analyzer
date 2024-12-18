3b076b391d4391b06df6fb1bf757a34d422be25b
benjamin.lesage <benjamin.lesage@onera.fr>
2024-12-18 19:12:29 +0100

    Remove loop between DMA and SRAMs in generated configurations

--- src/test/scala/onera/pmlanalyzer/pml/experiments/hbus/HbusCl2C2B8RoutingConstraints.scala
+++ src/test/scala/onera/pmlanalyzer/pml/experiments/hbus/HbusCl2C2B8RoutingConstraints.scala
@@ -8,7 +8,7 @@ import scala.language.postfixOps
 trait HbusCl2C2B8RoutingConstraints {
   self: HbusCl2C2B8Platform =>
 
-  val dma_targets = Seq(
+  private val dma_targets: Seq[Target] = Seq(
     rosace.ddr.BK0,
     rosace.ddr.BK1,
     rosace.ddr.BK2,
@@ -20,23 +20,26 @@ trait HbusCl2C2B8RoutingConstraints {
     rosace.eth,
   )
 
-  val cluster_inputs = Seq(
+  private val cluster_inputs: Seq[Hardware] = Seq(
     rosace.cg0.cl0.input_port,
     rosace.cg0.cl1.input_port,
     rosace.cg0.input_port,
   )
 
-  val cluster_outputs = Seq(
+  private val cluster_outputs: Seq[Hardware] = Seq(
     rosace.cg0.cl0.output_port,
     rosace.cg0.cl1.output_port,
     rosace.cg0.output_port,
   )
 
-  val cores = Seq(
+  private val cores: Seq[Initiator] = Seq(
     rosace.cg0.cl0.C0,
     rosace.cg0.cl0.C1,
     rosace.cg0.cl1.C0,
     rosace.cg0.cl1.C1,
+  )
+
+  private val srams: Seq[Target] = Seq(
   )
 
   for {
@@ -53,6 +56,14 @@ trait HbusCl2C2B8RoutingConstraints {
     in_port <- cluster_inputs
   } {
     i targeting target blockedBy in_port
+  }
+
+  for {
+    i <- Seq(rosace.dma)
+    target <- srams
+    out_port <- cluster_outputs
+  } {
+    i targeting target blockedBy out_port
   }
 
 }
