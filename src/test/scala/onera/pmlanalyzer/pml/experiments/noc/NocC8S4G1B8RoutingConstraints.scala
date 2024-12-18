0c20fdb07cc4d1b5910fd1455f50cf5faf7a90ef
benjamin.lesage <benjamin.lesage@onera.fr>
2024-12-18 19:12:29 +0100

    Remove loop between DMA and SRAMs in generated configurations

--- src/test/scala/onera/pmlanalyzer/pml/experiments/noc/NocC8S4G1B8RoutingConstraints.scala
+++ src/test/scala/onera/pmlanalyzer/pml/experiments/noc/NocC8S4G1B8RoutingConstraints.scala
@@ -8,7 +8,7 @@ import scala.language.postfixOps
 trait NocC8S4G1B8RoutingConstraints {
   self: NocC8S4G1B8Platform =>
 
-  val dma_targets = Seq(
+  private val dma_targets: Seq[Target] = Seq(
     rosace.ddr.BK0,
     rosace.ddr.BK1,
     rosace.ddr.BK2,
@@ -20,7 +20,7 @@ trait NocC8S4G1B8RoutingConstraints {
     rosace.eth,
   )
 
-  val cluster_inputs = Seq(
+  private val cluster_inputs: Seq[Hardware] = Seq(
     rosace.cg0.cl0.input_port,
     rosace.cg0.cl1.input_port,
     rosace.cg0.cl2.input_port,
@@ -32,7 +32,7 @@ trait NocC8S4G1B8RoutingConstraints {
     rosace.cg0.input_port,
   )
 
-  val cluster_outputs = Seq(
+  private val cluster_outputs: Seq[Hardware] = Seq(
     rosace.cg0.cl0.output_port,
     rosace.cg0.cl1.output_port,
     rosace.cg0.cl2.output_port,
@@ -44,7 +44,7 @@ trait NocC8S4G1B8RoutingConstraints {
     rosace.cg0.output_port,
   )
 
-  val cores = Seq(
+  private val cores: Seq[Initiator] = Seq(
     rosace.cg0.cl0.C0,
     rosace.cg0.cl1.C0,
     rosace.cg0.cl2.C0,
@@ -53,6 +53,9 @@ trait NocC8S4G1B8RoutingConstraints {
     rosace.cg0.cl5.C0,
     rosace.cg0.cl6.C0,
     rosace.cg0.cl7.C0,
+  )
+
+  private val srams: Seq[Target] = Seq(
   )
 
   for {
@@ -69,6 +72,14 @@ trait NocC8S4G1B8RoutingConstraints {
     in_port <- cluster_inputs
   } {
     i targeting target blockedBy in_port
+  }
+
+  for {
+    i <- Seq(rosace.dma)
+    target <- srams
+    out_port <- cluster_outputs
+  } {
+    i targeting target blockedBy out_port
   }
 
 }
