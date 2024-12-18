383c22d2f78b6d848476c43f8974d4770af5ea88
kevin-delmas <kevin.delmas@onera.fr>
2024-12-18 18:35:54 +0100

    add several topologies

--- src/test/scala/onera/pmlanalyzer/pml/experiments/GeneratedPlatforms.scala
+++ src/test/scala/onera/pmlanalyzer/pml/experiments/GeneratedPlatforms.scala
@@ -1,24 +1,28 @@
-/*******************************************************************************
- * Copyright (c)  2023. ONERA
- * This file is part of PML Analyzer
+/** *****************************************************************************
+ * Copyright (c) 2023. ONERA This file is part of PML Analyzer
  *
- * PML Analyzer is free software ;
- * you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation ;
- * either version 2 of  the License, or (at your option) any later version.
+ * PML Analyzer is free software ; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation ; either version 2 of the License, or (at your
+ * option) any later version.
  *
- * PML Analyzer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
+ * PML Analyzer is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY ; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License along with this program ;
- *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
- ******************************************************************************/
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program ; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 
 package onera.pmlanalyzer.pml.experiments
 
-import onera.pmlanalyzer.GnuPlotWriter
+// import onera.pmlanalyzer.GnuPlotWriter
+
+import onera.pmlanalyzer.pml.experiments.dbus.*
 import onera.pmlanalyzer.pml.experiments.hbus.*
+import onera.pmlanalyzer.pml.experiments.noc.*
 import onera.pmlanalyzer.pml.exporters.*
 import onera.pmlanalyzer.pml.model.configuration.TransactionLibrary
 import onera.pmlanalyzer.pml.model.hardware.Platform
@@ -51,58 +55,128 @@ class GeneratedPlatforms extends AnyFlatSpec with should.Matchers {
       with HbusCl2C4B8TransactionLibrary
       with TableBasedInterferenceSpecification {}
 
-  object HbusCl2C8B8
-      extends HbusCl2C8B8Platform
-      with HbusCl2C8B8Software
-      with HbusCl2C8B8TransactionLibrary
-      with TableBasedInterferenceSpecification {}
-
   object HbusCl4C2B8
       extends HbusCl4C2B8Platform
       with HbusCl4C2B8Software
       with HbusCl4C2B8TransactionLibrary
       with TableBasedInterferenceSpecification {}
 
-  object HbusCl4C4B8
-      extends HbusCl4C4B8Platform
-      with HbusCl4C4B8Software
-      with HbusCl4C4B8TransactionLibrary
+  object DbusC2D2B8
+    extends DbusC2D2B8Platform
+      with DbusC2D2B8Software
+      with DbusC2D2B8TransactionLibrary
       with TableBasedInterferenceSpecification {}
 
-  object HbusCl4C8B8
-      extends HbusCl4C8B8Platform
-      with HbusCl4C8B8Software
-      with HbusCl4C8B8TransactionLibrary
+  object DbusC2D4B8
+    extends DbusC2D4B8Platform
+      with DbusC2D4B8Software
+      with DbusC2D4B8TransactionLibrary
       with TableBasedInterferenceSpecification {}
 
-  object HbusCl8C2B8
-      extends HbusCl8C2B8Platform
-      with HbusCl8C2B8Software
-      with HbusCl8C2B8TransactionLibrary
+  object DbusC2D8B8
+    extends DbusC2D8B8Platform
+      with DbusC2D8B8Software
+      with DbusC2D8B8TransactionLibrary
       with TableBasedInterferenceSpecification {}
 
-  object HbusCl8C4B8
-      extends HbusCl8C4B8Platform
-      with HbusCl8C4B8Software
-      with HbusCl8C4B8TransactionLibrary
+  object DbusC4D2B8
+    extends DbusC4D2B8Platform
+      with DbusC4D2B8Software
+      with DbusC4D2B8TransactionLibrary
       with TableBasedInterferenceSpecification {}
 
-  object HbusCl8C8B8
-      extends HbusCl8C8B8Platform
-      with HbusCl8C8B8Software
-      with HbusCl8C8B8TransactionLibrary
+  object DbusC4D4B8
+    extends DbusC4D4B8Platform
+      with DbusC4D4B8Software
+      with DbusC4D4B8TransactionLibrary
+      with TableBasedInterferenceSpecification {}
+
+  object DbusC4D8B8
+    extends DbusC4D8B8Platform
+      with DbusC4D8B8Software
+      with DbusC4D8B8TransactionLibrary
+      with TableBasedInterferenceSpecification {}
+
+  object DbusC8D2B8
+    extends DbusC8D2B8Platform
+      with DbusC8D2B8Software
+      with DbusC8D2B8TransactionLibrary
+      with TableBasedInterferenceSpecification {}
+
+  object DbusC8D4B8
+    extends DbusC8D4B8Platform
+      with DbusC8D4B8Software
+      with DbusC8D4B8TransactionLibrary
+      with TableBasedInterferenceSpecification {}
+
+  object NocC4S2G1B8
+    extends NocC4S2G1B8Platform
+      with NocC4S2G1B8Software
+      with NocC4S2G1B8TransactionLibrary
+      with TableBasedInterferenceSpecification {}
+
+  object NocC4S2G2B8
+    extends NocC4S2G2B8Platform
+      with NocC4S2G2B8Software
+      with NocC4S2G2B8TransactionLibrary
+      with TableBasedInterferenceSpecification {}
+
+  object NocC4S4G1B8
+    extends NocC4S4G1B8Platform
+      with NocC4S4G1B8Software
+      with NocC4S4G1B8TransactionLibrary
+      with TableBasedInterferenceSpecification {}
+
+  object NocC4S4G2B8
+    extends NocC4S4G2B8Platform
+      with NocC4S4G2B8Software
+      with NocC4S4G2B8TransactionLibrary
+      with TableBasedInterferenceSpecification {}
+
+  object NocC8S2G1B8
+    extends NocC8S2G1B8Platform
+      with NocC8S2G1B8Software
+      with NocC8S2G1B8TransactionLibrary
+      with TableBasedInterferenceSpecification {}
+
+  object NocC8S2G2B8
+    extends NocC8S2G2B8Platform
+      with NocC8S2G2B8Software
+      with NocC8S2G2B8TransactionLibrary
+      with TableBasedInterferenceSpecification {}
+
+  object NocC8S4G1B8
+    extends NocC8S4G1B8Platform
+      with NocC8S4G1B8Software
+      with NocC8S4G1B8TransactionLibrary
+      with TableBasedInterferenceSpecification {}
+
+  object NocC8S4G2B8
+    extends NocC8S4G2B8Platform
+      with NocC8S4G2B8Software
+      with NocC8S4G2B8TransactionLibrary
       with TableBasedInterferenceSpecification {}
 
   private val platforms = Seq(
     HbusCl2C2B8,
     HbusCl2C4B8,
-    HbusCl2C8B8,
     HbusCl4C2B8,
-    HbusCl4C4B8,
-    HbusCl4C8B8,
-    HbusCl8C2B8,
-    HbusCl8C4B8,
-    HbusCl8C8B8
+    DbusC2D2B8,
+    DbusC2D4B8,
+    DbusC2D8B8,
+    DbusC4D2B8,
+    DbusC4D4B8,
+    DbusC4D8B8,
+    DbusC8D2B8,
+    DbusC8D4B8,
+    NocC4S2G1B8,
+    NocC4S2G2B8,
+    NocC4S4G1B8,
+    NocC4S4G2B8,
+    NocC8S2G1B8,
+    NocC8S2G2B8,
+    NocC8S4G1B8,
+    NocC8S4G2B8
   )
 
   "Generated architectures" should "be exportable" in {
