2a9ba79f7650d823f825251d1e45a63c922a81d4
Camille Coquand <camille.coquand@onera.fr>
2025-02-05 14:47:02 +0100

    Add InterferenceTest.scala file, does not compile

--- src/test/scala/onera/pmlanalyzer/views/interference/operators/InterferenceTest.scala
+++ src/test/scala/onera/pmlanalyzer/views/interference/operators/InterferenceTest.scala
@@ -1,7 +1,7 @@
 package onera.pmlanalyzer.views.operators
 
 import onera.pmlanalyzer.views.operators.*
-import onera.pmlanalyzer.pml.operators.* 
+import onera.pmlanalyzer.pml.operators.*
 import onera.pmlanalyzer.pml.model.hardware.*
 import onera.pmlanalyzer.views.interference.model.specification.PhysicalTableBasedInterferenceSpecification
 import onera.pmlanalyzer.views.interference.operators.*
@@ -11,7 +11,7 @@ import onera.pmlanalyzer.views.interference.model.relations.{
   NotInterfereRelation
 }
 
-import sourcecode.Name 
+import sourcecode.Name
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.flatspec.AnyFlatSpecLike
 import org.scalatest.matchers.should
@@ -24,36 +24,44 @@ class InterferenceTest extends AnyFlatSpecLike with should.Matchers {
   object PlatformFixture extends Platform(Symbol("fixture"))
 
   import PlatformFixture.*
-    // "Two Hardwares" should "be able to interact with each other"
-    "Two Hardwares" should "be able to interact with each other" in {
-      val i1: Initiator = Initiator()
-      val i2: Initiator = Initiator()
-      val st1: SimpleTransporter = SimpleTransporter()
-      val st2: SimpleTransporter = SimpleTransporter()
-      val t1: Target = Target()
-      val t2: Target = Target() 
+  // "Two Hardwares" should "be able to interact with each other"
+  "Two Hardwares" should "be able to interact with each other" in {
+    val i1: Initiator = Initiator()
+    val i2: Initiator = Initiator()
+    val st1: SimpleTransporter = SimpleTransporter()
+    val st2: SimpleTransporter = SimpleTransporter()
+    val t1: Target = Target()
+    val t2: Target = Target()
 
-      for ((a,b) <- List(i1, i2, st1, st2, t1, t2).zip(List(i1, i2, st1, st2, t1, t2))) {
-        a interfereWith b
+    for (
+      (a, b) <- List(i1, i2, st1, st2, t1, t2).zip(
+        List(i1, i2, st1, st2, t1, t2)
+      )
+    ) {
+      a interfereWith b
 
-      }
     }
+  }
 
-    "Two Hardwares" should "be able not to interact with each other" in {
-      val i1: Initiator = Initiator()
-      val i2: Initiator = Initiator()
-      val st1: SimpleTransporter = SimpleTransporter()
-      val st2: SimpleTransporter = SimpleTransporter()
-      val t1: Target = Target()
-      val t2: Target = Target() 
+  "Two Hardwares" should "be able not to interact with each other" in {
+    val i1: Initiator = Initiator()
+    val i2: Initiator = Initiator()
+    val st1: SimpleTransporter = SimpleTransporter()
+    val st2: SimpleTransporter = SimpleTransporter()
+    val t1: Target = Target()
+    val t2: Target = Target()
 
-      for ((a,b) <- List(i1, i2, st1, st2, t1, t2).zip(List(i1, i2, st1, st2, t1, t2))) {
-        a notInterfereWith b
+    for (
+      (a, b) <- List(i1, i2, st1, st2, t1, t2).zip(
+        List(i1, i2, st1, st2, t1, t2)
+      )
+    ) {
+      a notInterfereWith b
 
-      }
     }
+  }
 
-    // "A Hardware" should "be able to have interfering services"
-    // "A Hardware" should "be able to have non-interfering services"
+  // "A Hardware" should "be able to have interfering services"
+  // "A Hardware" should "be able to have non-interfering services"
 }
 // Tests seulement pour la relation Hadrware/Hardware ?
