7d0767be2c66f6e50aaaeaaaf9ff69ae06a1c2be
kevin-delmas <kevin.delmas@onera.fr>
2025-02-12 14:14:05 +0100

    Add an exporter for graph reduction

--- src/test/scala/onera/pmlanalyzer/pml/experiments/GeneratedPlatforms.scala
+++ src/test/scala/onera/pmlanalyzer/pml/experiments/GeneratedPlatforms.scala
@@ -218,6 +218,8 @@ class GeneratedPlatforms extends AnyFlatSpec with should.Matchers {
         computeSemantics = false,
         onlySummary = true
       )
+      p.exportSemanticReduction()
+      p.exportGraphReduction()
     }
   }
 }
