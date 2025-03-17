c10ff3828defe74608b464e58aafea9e18159a4d
Benjamin Lesage <m.aqpl.c@gmail.com>
2025-03-17 09:50:36 +0100

    Fix missing transaction interference specification

---
+++ src/test/scala/onera/pmlanalyzer/pml/experiments/GenericTransactionInterferenceSpecification.scala
@@ -0,0 +1,10 @@
+package onera.pmlanalyzer.pml.experiments
+
+import onera.pmlanalyzer.views.interference.model.specification.PhysicalTableBasedInterferenceSpecification
+import onera.pmlanalyzer.pml.operators.*
+import onera.pmlanalyzer.views.interference.operators.*
+
+trait GenericTransactionInterferenceSpecification
+  extends PhysicalTableBasedInterferenceSpecification {
+  self: GenericPlatform =>
+}
