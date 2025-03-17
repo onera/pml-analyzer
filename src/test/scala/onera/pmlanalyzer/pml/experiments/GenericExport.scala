69591d0df312db10f34f7f881f5380830d71e132
Benjamin Lesage <m.aqpl.c@gmail.com>
2025-03-17 11:43:13 +0100

    Draft simplified Generic platform generator

--- src/test/scala/onera/pmlanalyzer/pml/experiments/GenericExport.scala
+++ src/test/scala/onera/pmlanalyzer/pml/experiments/GenericExport.scala
@@ -18,6 +18,8 @@
 package onera.pmlanalyzer.pml.experiments
 
 import onera.pmlanalyzer.pml.exporters.*
+import onera.pmlanalyzer.pml.model.configuration.TransactionLibrary
+import onera.pmlanalyzer.pml.model.hardware.Platform
 import onera.pmlanalyzer.pml.model.utils.Message
 import onera.pmlanalyzer.pml.operators.*
 import onera.pmlanalyzer.views.interference.examples.mySys.{
@@ -25,92 +27,134 @@ import onera.pmlanalyzer.views.interference.examples.mySys.{
   MySysInterferenceSpecification
 }
 import onera.pmlanalyzer.views.interference.exporters.*
+import onera.pmlanalyzer.views.interference.model.specification.{
+  ApplicativeTableBasedInterferenceSpecification,
+  PhysicalTableBasedInterferenceSpecification
+}
 import onera.pmlanalyzer.views.interference.operators.*
 
 /** Program entry point to export several version of Keystone
   */
 object GenericExport extends App {
 
-  /** Keystone where all transactions are considered
-    *
-    * @group platform_def
-    */
-  //    extends GenericPlatform(
-  //      name = Symbol("GenericSample"),
-  //      nbGroupDSP = 1,
-  //      nbGroupCore = 1,
-  //      nbClusterGroupDSP =
-  //        1, // FIXME This input seems redundant with the DSP Per Group
-  //      nbClusterGroupCore =
-  //        1, // FIXME This input seems redundant with the Core Per Group
-  //      nbClusterDSPPerGroup = 2,
-  //      nbClusterCorePerGroup = 2,
-  //      nbDSPPerCluster = 2,
-  //      nbCorePerCluster = 2,
-  //      nbDDRBank = 8,
-  //      nbDDRController = 1
-  //    )
-  object GenericSample
-      extends GenericPlatform(
-        name = Symbol("GenericSample"),
-        nbGroupDSP = 1,
-        nbGroupCore = 0,
-        nbClusterGroupDSP =
-          1, // FIXME This input seems redundant with the DSP Per Group
-        nbClusterGroupCore =
-          1, // FIXME This input seems redundant with the Core Per Group
-        nbClusterDSPPerGroup = 1,
-        nbClusterCorePerGroup = 2,
-        nbDSPPerCluster = 1,
-        nbCorePerCluster = 2,
-        nbDDRBank = 8,
-        nbDDRController = 1
-      )
-      with GenericSoftware
+  def generatePlatformFromConfiguration(
+      coreCount: Int,
+      dspCount: Int,
+      ddrPartitions: Int,
+      coresPerBankPerPartition: Int
+  ): Platform
+    with TransactionLibrary
+    with PhysicalTableBasedInterferenceSpecification
+    with ApplicativeTableBasedInterferenceSpecification = {
+    // FIXME Assert ddrPartitions <= GP Core Count
+    // FIXME Assert GP Core count is multiple of ddrPartitions
+
+    val gp_cores: Int = coreCount
+    val dsp_cores: Int = dspCount
+
+    // Create the required GP groups, based on target partition count
+    // FIXME How to configure more than 1 GP cluster per group?
+    val gp_group_count: Int = ddrPartitions
+    val gp_cluster_per_group: Int = 1
+    val gp_cores_per_cluster = gp_cores / gp_group_count / gp_cluster_per_group
+
+    // Allocate banks to groups and cores
+    val bank_count: Int =
+      Math.ceil(gp_cores / ddrPartitions / coresPerBankPerPartition).toInt
+
+    // Create a single DSP group
+    // - No activity from DSP Cores to the outside
+    // - Only the (single) DMA comes into the DSP clusters
+    val dsp_group_count: Int = 1
+    val dsp_cluster_per_group: Int = 1
+    val dsp_cores_per_cluster: Int =
+      dsp_cores / dsp_group_count / dsp_cluster_per_group
+
+    // Derive additional Generic Platform parameters
+    val name = Symbol(
+      s"GenericSample_C${gp_cores}_D${dsp_cores}_P${ddrPartitions}_PerBank${coresPerBankPerPartition}"
+    )
+    val ddr_count: Int = ddrPartitions
+
+    new GenericPlatform(
+      name = name,
+      nbGroupCore = gp_group_count,
+      nbGroupDSP = dsp_group_count,
+      nbClusterGroupDSP =
+        1, // FIXME This input seems redundant with the DSP Per Group
+      nbClusterGroupCore =
+        1, // FIXME This input seems redundant with the Core Per Group
+      nbClusterCorePerGroup = gp_cluster_per_group,
+      nbClusterDSPPerGroup = dsp_cluster_per_group,
+      nbCorePerCluster = gp_cores_per_cluster,
+      nbDSPPerCluster = dsp_cores_per_cluster,
+      nbDDRBank = bank_count,
+      nbDDRController = ddr_count
+    ) with GenericSoftware
       with GenericTransactionLibrary
       with GenericRoutingConstraints
-//      with MyProcInterferenceSpecification
-//      with MySysInterferenceSpecification
-
-  // Export only HW used by SW (explicit)
-  GenericSample.exportRestrictedHWAndSWGraph()
-
-  // Export HW and SW graph whether used or not
-  GenericSample.exportHWAndSWGraph()
-
-  // Export Service graph whether used or not and considering that all services are non-exclusive
-  GenericSample.exportServiceGraph()
-
-  // Export Service graph considering and SW
-  GenericSample.exportRestrictedServiceAndSWGraph()
-
-  // Export Service graph considering that all services are non-exclusive
-  GenericSample.exportServiceGraphWithInterfere()
-
-  // Export individually the Service graph of each software
-  GenericSample.applications foreach { s =>
-    GenericSample.exportRestrictedServiceGraphForSW(s)
+      with GenericTransactionInterferenceSpecification
+      with GenericApplicationInterferenceSpecification
   }
 
-  // Export the application allocation table
-  GenericSample.exportAllocationTable()
+  val log2 = (x: Int) => (Math.log10(x) / Math.log10(2.0)).toInt
+  val candidatePlatforms = for {
+    coreCount <- Seq(1, 2, 4, 8)
+    dspCount <- Seq(0, 1, 2, 4, 8)
+    ddrPartitions <- for {i <- 0 until log2(coreCount) + 1} yield Math.pow(2.0, i).toInt
+    coresPerBankPerPartition <- for {i <- 0 until log2(coreCount / ddrPartitions) + 1} yield Math.pow(2.0, i).toInt
+    if (coreCount + dspCount < 8)
+  } yield {
+    generatePlatformFromConfiguration(
+      coreCount = coreCount,
+      dspCount = dspCount,
+      ddrPartitions = ddrPartitions,
+      coresPerBankPerPartition = coresPerBankPerPartition
+    )
+  }
 
-  // Export the data allocation table
-  GenericSample.exportDataAllocationTable()
+  for (platform <- candidatePlatforms) {
+    println(platform.name.name)
+      // Export only HW used by SW (explicit)
+      platform.exportRestrictedHWAndSWGraph()
 
-  // Export the target used by software
-  GenericSample.exportSWTargetUsageTable()
+      // Export HW and SW graph whether used or not
+      platform.exportHWAndSWGraph()
 
-  // Export the routing constraints
-  GenericSample.exportRouteTable()
+      // Export Service graph whether used or not and considering that all services are non-exclusive
+      platform.exportServiceGraph()
 
-  // Export the deactivated components
-  GenericSample.exportDeactivatedComponents()
+      // Export Service graph considering and SW
+      platform.exportRestrictedServiceAndSWGraph()
 
-  // Export the transactions defined by the user
-  GenericSample.exportUserScenarios()
+      // Export Service graph considering that all services are non-exclusive
+      platform.exportServiceGraphWithInterfere()
 
-//  GenericSample.exportSemanticsSize()
+      // Export individually the Service graph of each software
+      platform.applications foreach { s =>
+        platform.exportRestrictedServiceGraphForSW(s)
+      }
 
-  //  GenericSample.exportAnalysisGraph()
+      // Export the application allocation table
+      platform.exportAllocationTable()
+
+      // Export the data allocation table
+      platform.exportDataAllocationTable()
+
+      // Export the target used by software
+      platform.exportSWTargetUsageTable()
+
+      // Export the routing constraints
+      platform.exportRouteTable()
+
+      // Export the deactivated components
+      platform.exportDeactivatedComponents()
+
+      // Export the transactions defined by the user
+      platform.exportUserScenarios()
+
+      platform.exportSemanticsSize()
+
+      platform.exportAnalysisGraph()
+  }
 }
