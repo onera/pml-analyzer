c9e6b87ea1b888566310c3389998a620247fe006
Camille Coquand <camille.coquand@onera.fr>
2025-04-07 17:06:34 +0200

    #5 - Add tests for Petri nets

--- src/test/scala/onera/pmlanalyzer/views/interference/model/formalisation/Petri/TransitionTest.scala
+++ src/test/scala/onera/pmlanalyzer/views/interference/model/formalisation/Petri/TransitionTest.scala
@@ -16,57 +16,38 @@
   * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
   */
 
-package onera.pmlanalyzer.views.interference.model.formalisation.Petri
+package onera.pmlanalyzer.views.interference.formalisation.Petri
 
 import onera.pmlanalyzer.views.interference.model.formalisation.Petri.Place
 import onera.pmlanalyzer.views.interference.model.formalisation.Petri.Transition
-import onera.pmlanalyzer.views.interference.model.formalisation.Petri.Place.*
 import scala.collection.mutable
 import sourcecode.Name
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
-import org.scalacheck.{Arbitrary, Gen}
-
-trait TransitionGenTest extends PlaceGenTest {
-  val markingGen: Gen[Marking.Marking] =
-    for {
-      places <- Gen.listOf(genPlace)
-      int <- Gen.listOfN(places.size, Gen.choose(1, 10))
-    } yield mutable.Map(places.zip(int): _*)
-
-  val transitionGen: Gen[Transition] =
-    for {
-      name <- Gen.identifier
-      pre <- markingGen
-      post <- markingGen
-    } yield Transition(name, pre, post)
-}
 
 class TransitionTest extends AnyFlatSpec with should.Matchers {
-
-  // TODO Provide a generator for Transition
 
   val p0: Place = Place("p0")
   val p1: Place = Place("p1")
   val t0: Transition = Transition(
     "t0",
-    Marking(p0 -> 1, p1 -> 2),
-    Marking(p0 -> 2, p1 -> 1)
+    mutable.Map(p0 -> 1, p1 -> 2),
+    mutable.Map(p0 -> 2, p1 -> 1)
   )
-  val t1: Transition = Transition("t1", Marking.empty, Marking.empty)
+  val t1: Transition = Transition("t1", mutable.Map.empty, mutable.Map.empty)
 
   "A transition" should "have a name" in {
-    t0.name should be("t0")
+    t0.name shouldBe "t0"
   }
   it should "have a Pre and a Post map" in {
-    t0.pre(p0) should be(1)
-    t0.pre(p1) should be(2)
-    t0.post(p1) should be(1)
-    t0.post(p0) should be(2)
+    t0.Pre(p0) shouldBe 1
+    t0.Pre(p1) shouldBe 2
+    t0.Post(p1) shouldBe 1
+    t0.Post(p0) shouldBe 2
   }
 
   "A transition" should "be able to admit empty Pre or Post condition" in {
-    t1.pre should be(empty)
-    t1.post should be(empty)
+    t1.Pre.isEmpty shouldBe true
+    t1.Post.isEmpty shouldBe true
   }
 }
