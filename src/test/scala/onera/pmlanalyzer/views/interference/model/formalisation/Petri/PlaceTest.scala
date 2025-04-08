fcb2dc363e882342c2a3e1234a90eaeb376a1ab2
kevin-delmas <kevin.delmas@onera.fr>
2025-04-08 15:55:37 +0200

    Refactoring

--- src/test/scala/onera/pmlanalyzer/views/interference/model/formalisation/Petri/PlaceTest.scala
+++ src/test/scala/onera/pmlanalyzer/views/interference/model/formalisation/Petri/PlaceTest.scala
@@ -35,6 +35,8 @@ class PlaceTest extends AnyFlatSpec with PlaceGenTest with should.Matchers {
 
   // TODO provide a generator of Places for tests
 
+  //TODO provide a generator of Places for tests
+
   val p1: Place = Place("p1")
 
   "A place" should "have a name" in {
