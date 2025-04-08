3576f1727560652d607e31d99a8e397c95cfb28e
kevin-delmas <kevin.delmas@onera.fr>
2025-04-08 16:21:31 +0200

    Add example to get all transactions from a platform

---
+++ src/test/scala/onera/pmlanalyzer/views/interference/model/formalisation/Petri/MySysTest.scala
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c)  2023. ONERA
+ * This file is part of PML Analyzer
+ *
+ * PML Analyzer is free software ;
+ * you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation ;
+ * either version 2 of  the License, or (at your option) any later version.
+ *
+ * PML Analyzer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program ;
+ *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ ******************************************************************************/
+
+package onera.pmlanalyzer.views.interference.model.formalisation.Petri
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+import onera.pmlanalyzer.pml.examples.mySys.MySysExport.MySys
+
+class MySysTest extends AnyFlatSpec with should.Matchers {
+
+  import MySys.*
+
+  "MySys" should "be transformable into PetriNet" in {
+    for {
+      (sId, tIds) <- scenarioByUserName
+    } {
+      println(s"$sId contains:")
+      for {
+        tId <- tIds
+        sPath <- purifiedTransactions.get(tId)
+        tName <- transactionUserName.getOrElse(tId, Set(tId))
+      } {
+        println(s"$tName: ${sPath.mkString("::")}")
+      }
+    }
+  }
+}
