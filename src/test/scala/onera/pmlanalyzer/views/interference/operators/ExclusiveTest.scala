a8b5867407f24f1db03d0b016fcb0d83411ebef5
Camille Coquand <camille.coquand@onera.fr>
2025-04-14 14:31:42 +0200

    #5 - Delete unecessary imports
    # Conflicts:
    #	src/test/scala/onera/pmlanalyzer/views/interference/operators/ExclusiveTest.scala

--- src/test/scala/onera/pmlanalyzer/views/interference/operators/ExclusiveTest.scala
+++ src/test/scala/onera/pmlanalyzer/views/interference/operators/ExclusiveTest.scala
@@ -2,6 +2,12 @@ package onera.pmlanalyzer.views.interference.operators
 
 import onera.pmlanalyzer.pml.model.configuration.*
 import onera.pmlanalyzer.pml.model.hardware.*
+import onera.pmlanalyzer.views.interference.model.specification.{
+  ApplicativeTableBasedInterferenceSpecification,
+  PhysicalTableBasedInterferenceSpecification
+}
+import onera.pmlanalyzer.pml.model.configuration.TransactionLibrary
+import onera.pmlanalyzer.pml.model.configuration.TransactionLibrary.UserTransactionId
 import onera.pmlanalyzer.pml.model.software.{Application, Data}
 import onera.pmlanalyzer.pml.operators.*
 import onera.pmlanalyzer.pml.operators.Transform.TransactionLibraryInstances
@@ -9,11 +15,16 @@ import onera.pmlanalyzer.views.interference.InterferenceTestExtension.UnitTests
 import onera.pmlanalyzer.views.interference.model.specification.ApplicativeTableBasedInterferenceSpecification
 import onera.pmlanalyzer.views.interference.model.specification.InterferenceSpecification.AtomicTransactionId
 import onera.pmlanalyzer.views.interference.operators.*
+import onera.pmlanalyzer.pml.model.hardware.Platform
+import onera.pmlanalyzer.views.interference.model.specification.InterferenceSpecification.PhysicalTransactionId
+import sourcecode.Name
+import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.flatspec.AnyFlatSpecLike
 import org.scalatest.matchers.should
 import sourcecode.Name
 
 import scala.language.postfixOps
+import onera.pmlanalyzer.pml.model.configuration.TransactionLibrary.UserTransactionId
 
 class ExclusiveTest extends AnyFlatSpecLike with should.Matchers {
 
