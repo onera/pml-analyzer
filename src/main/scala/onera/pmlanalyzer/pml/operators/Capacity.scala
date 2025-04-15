4c0db03e589b1c1ebf0d2da18fd971117935ceb1
Camille Coquand <camille.coquand@onera.fr>
2025-04-15 16:37:58 +0200

    #5 - Error with capac ity operator

--- src/main/scala/onera/pmlanalyzer/pml/operators/Capacity.scala
+++ src/main/scala/onera/pmlanalyzer/pml/operators/Capacity.scala
@@ -1,68 +1,65 @@
-/*******************************************************************************
- * Copyright (c)  2023. ONERA
- * This file is part of PML Analyzer
- *
- * PML Analyzer is free software ;
- * you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation ;
- * either version 2 of  the License, or (at your option) any later version.
- *
- * PML Analyzer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along with this program ;
- *  if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
- ******************************************************************************/
+/** *****************************************************************************
+  * Copyright (c) 2023. ONERA This file is part of PML Analyzer
+  *
+  * PML Analyzer is free software ; you can redistribute it and/or modify it
+  * under the terms of the GNU Lesser General Public License as published by the
+  * Free Software Foundation ; either version 2 of the License, or (at your
+  * option) any later version.
+  *
+  * PML Analyzer is distributed in the hope that it will be useful, but WITHOUT
+  * ANY WARRANTY ; without even the implied warranty of MERCHANTABILITY or
+  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+  * for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public License
+  * along with this program ; if not, write to the Free Software Foundation,
+  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+  */
 
-package onera.pmlanalyzer.pml.operators
+package onera.pmlanalyzer.pml.model.operators
 
-import onera.pmlanalyzer.pml.operators.*
 import onera.pmlanalyzer.pml.model.service.Service
-import onera.pmlanalyzer.pml.model.hardware.Hardware
 import onera.pmlanalyzer.views.interference.model.specification.InterferenceSpecification.PhysicalTransactionId
-import onera.pmlanalyzer.pml.model.relations.{
-  DemandRelation,
-  CapacityRelation,
-  ProvideRelation
-}
-import sourcecode.{File, Line}
+import onera.pmlanalyzer.pml.model.relations.DemandRelation
+import onera.pmlanalyzer.pml.model.relations.CapacityRelation
 
-private[operators] trait Capacity[L, R] {
-  def apply(l: L, r: R)(using line: Line, file: File): Unit
+private[operators] trait Capacity[T] {
+  def apply(l: T, r: T): Unit
 }
 
 object Capacity {
 
   /** If an element l of type T can be exclusive with another element r of type
-   * T, the following operator can be used {{{l exclusiveWith r}}}
-   */
+    * T, the following operator can be used {{{l exclusiveWith r}}}
+    */
   trait Ops {
-    extension [L](l: L) {
-      def hasCapacity[R](
-          r: R
-      )(using d: Capacity[L, R], line: Line, file: File): Unit =
-        d(l, r)
+
+    extension (t: PhysicalTransactionId) {
+      def hasDemand(d: Int)(using dr: DemandRelation): Unit =
+        // dr.hasDemand(t, d)
+        dr.demandOfTransaction += (t -> d)
     }
+
+    extension (s: Service) {
+      def hasCapacity(c: Int)(using cr: CapacityRelation): Unit =
+        // cr.hasCapacity(s, c)
+        cr.capacityOfService += (s -> c)
+    }
+
   }
 
-  given [LH <: Hardware, R](using
-      c: Capacity[Service, R],
-      pr: Provided[LH, Service]
-  ): Capacity[LH, R] with {
-    def apply(l: LH, r: R)(using line: Line, file: File): Unit =
-      for {
-        s <- l.services
-      } yield s hasCapacity r
-  }
+  // given [PhysicalTransactionId, Int](using
+  //     dr: DemandRelation
+  // ): DemandRelation with {
+  //   def hasDemand(t: PhysicalTransactionId, d: Int): Unit =
+  //     dr.demandOfTransaction += (t -> d)
+  // }
 
-  /**
-   * We can generate a proof that a capacity of type R is assignable to a type L
-   * If we can find a relation containing syper types of L and R
-   */
-  given [CL, CR, L <: CL, R <: CR](using
-      cr: CapacityRelation[CL, CR]
-  ): Capacity[L, R] with {
-    def apply(l: L, r: R)(using line: Line, file: File): Unit = cr.add(l, r)
-  }
+  // given [LS <: Service, Int](using
+  //     cr: CapacityRelation
+  // ): CapacityRelation with {
+  //   def hasCapacity(s: PhysicalTransactionId, c: Int): Unit =
+  //     cr.demandOfTransaction += (s -> c)
+  // }
+
 }
