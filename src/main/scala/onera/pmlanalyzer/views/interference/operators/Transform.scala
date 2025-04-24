44f707c211a539030718378a882b693479ce82ec
Camille Coquand <camille.coquand@gmail.com>
2025-04-24 13:38:58 +0200

    #5 - Add tests for Capacity operator and Demand operator (not complete)

--- src/main/scala/onera/pmlanalyzer/views/interference/operators/Transform.scala
+++ src/main/scala/onera/pmlanalyzer/views/interference/operators/Transform.scala
@@ -25,9 +25,11 @@ import onera.pmlanalyzer.pml.model.configuration.TransactionLibrary.{
 }
 import onera.pmlanalyzer.pml.model.hardware.Platform
 import onera.pmlanalyzer.pml.model.software.Application
+import onera.pmlanalyzer.views.interference.model.specification.InterferenceSpecification
 import onera.pmlanalyzer.views.interference.model.specification.InterferenceSpecification.{
   PhysicalTransaction,
-  PhysicalTransactionId
+  PhysicalTransactionId,
+  PhysicalScenarioId
 }
 
 trait Transform[L, R] {
@@ -40,16 +42,18 @@ object Transform {
     self: Platform =>
 
     /** Convert a physical id to the corresponding path of services
-      * @group transform_operator
-      */
+     *
+     * @group transform_operator
+     */
     given Transform[PhysicalTransactionId, Option[PhysicalTransaction]] with {
       def apply(l: PhysicalTransactionId): Option[PhysicalTransaction] =
         transactionsByName.get(l)
     }
 
     /** Convert an application to the set of transaction id its trigger
-      * @group transform_operator
-      */
+     *
+     * @group transform_operator
+     */
     given Transform[Application, Set[PhysicalTransactionId]] with {
       def apply(l: Application): Set[PhysicalTransactionId] =
         transactionsBySW.getOrElse(l, Set.empty)
@@ -60,35 +64,52 @@ object Transform {
     self: TransactionLibrary & Platform =>
 
     /** Convert a user transaction to its physical transaction id
-      * @group transform_operator
-      */
+     *
+     * @group transform_operator
+     */
     given Transform[Transaction, Option[PhysicalTransactionId]] with {
       def apply(l: Transaction): Option[PhysicalTransactionId] =
         transactionByUserName.get(l.userName)
     }
 
     /** Convert a user defined scenario to the set of its physical scenario ids
-      * @group transform_operator
-      */
+     *
+     * @group transform_operator
+     */
     given Transform[Scenario, Set[PhysicalTransactionId]] with {
       def apply(l: Scenario): Set[PhysicalTransactionId] =
         scenarioByUserName.getOrElse(l.userName, Set.empty)
     }
 
     /** Convert a user transaction id to its physical transaction id
-      * @group transform_operator
-      */
+     *
+     * @group transform_operator
+     */
     given Transform[UserTransactionId, Option[PhysicalTransactionId]] with {
       def apply(l: UserTransactionId): Option[PhysicalTransactionId] =
         transactionByUserName.get(l)
     }
 
     /** Convert a user scenario id to the set of its physical scenario ids
-      * @group transform_operator
-      */
+     *
+     * @group transform_operator
+     */
     given Transform[UserScenarioId, Set[PhysicalTransactionId]] with {
       def apply(l: UserScenarioId): Set[PhysicalTransactionId] =
         scenarioByUserName.getOrElse(l, Set.empty)
+    }
+  }
+
+  trait InterferenceSpecificationInstances {
+    self: InterferenceSpecification & Platform =>
+
+    /** Convert a physical scenario id to the set of its physical transactions ids
+     *
+     * @group transform_operator
+     */
+    given Transform[PhysicalScenarioId, Set[PhysicalTransactionId]] with {
+      def apply(l: PhysicalScenarioId): Set[PhysicalTransactionId] =
+        purifiedScenarios.getOrElse(l, Set.empty)
     }
   }
 }
